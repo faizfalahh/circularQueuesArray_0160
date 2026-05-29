d31aedc2fdc044d789c3f64595595db40bef171f
faizfalahh <faizfalah38@gmail.com>
2026-05-29 17:19:01 +0800

    menambahkan proses input queue

--- circularQueuesArray.cpp
+++ circularQueuesArray.cpp
@@ -8,10 +8,17 @@ private:
     int FRONT, REAR; 
     int queue_array[max]; 
  
-    public: 
+public: 
     Queues() 
     { 
         FRONT = -1; 
         REAR = -1; 
     } 
  
+     
+    void insert() 
+    { 
+        int num; 
+        cout << "Enter a number: "; 
+        cin >> num; 
+        cout << endl; 
